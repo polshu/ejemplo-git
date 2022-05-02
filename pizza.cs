dd400f610b9ed7141d23df45b7392858977e66a9
Android Studio <devandroid@ort>
2022-05-02 08:58:49 -0300

    Cambio en nueva rama

--- pizza.cs
+++ pizza.cs
@@ -6,6 +6,6 @@ namespace Pizzas.API.Models {
         public string   Nombre          { get; set; }
         public bool     LibreGluten     { get; set; }
         public float    Sueldo          { get; set; }
-        public string   Descripcion     { get; set; }
+        public string   Descripcion     { get; set; } /// Es ewn nueva rama
     }
 }
